567e513c9c7da17582c798d6c1b571ca01103c8d
margare-jihua <margaret.jihua@gmail.com>
2020-11-04 23:01:21 -0800

    Add Contact page, edit home page and layout

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace RazorPagesIntro.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; private set; } = "PageModel in C#";
+
+        public void OnGet()
+        {
+            Message += $" Server time is { DateTime.Now }";
+        }
+    }
+}
