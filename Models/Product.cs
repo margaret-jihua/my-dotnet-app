c4ab672b261bb297b51d63562cb00226ea551fd8
margare-jihua <margaret.jihua@gmail.com>
2020-11-10 17:54:16 -0800

    issue with JsonSerializer

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myWebApp.Models
 {
     public class Product
     {
         public string Id { get; set; }
         public string Maker { get; set; }
-        public string Image { get; set; }
         [JsonPropertyName("img")]
+        public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using myWebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace myWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
